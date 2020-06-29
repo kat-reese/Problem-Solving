ebbcfd7a902d0b491bad2773a0a9103a231774e2
kat09tails <kathryn.reese@umontana.edu>
2020-06-29 13:32:07 -0600

    added sum_to_n

---
+++ sum_to_n.py
@@ -0,0 +1,17 @@
+class sum_to_n:
+    def __init__(self):
+        try:
+            n = int(input("Enter a whole number: >>> "))
+        except:
+            print("Please enter a whole number!")
+        print("Your number is %d." % n)
+        sum = 0
+        for i in range(1, n + 1):
+            sum += i
+        print("The sum of 1 to %d is %d" % (n , sum))
+
+def main():
+    one_to_n = sum_to_n()
+
+if __name__ == "__main__":
+    main()
